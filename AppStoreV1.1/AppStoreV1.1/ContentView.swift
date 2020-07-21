b5dbad5c81303b6052601fb212290dbba7ca4c7e
Avisa Poshtkouhi <aposhtkouhi@gmail.com>
2020-07-21 11:37:52 -0700

    Add BaseTabBarController to Project

---
+++ AppStoreV1.1/AppStoreV1.1/ContentView.swift
@@ -0,0 +1,37 @@
+//
+//  ContentView.swift
+//  AppStoreV1.1
+//
+//  Created by Avisa Poshtkouhi on 7/21/20.
+//  Copyright © 2020 Avisa Poshtkouhi. All rights reserved.
+//
+
+import SwiftUI
+
+class BaseTabBarController: UITabBarController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        Container().edgesIgnoringSafeArea(.all)
+    }
+}
+
+struct Container: UIViewControllerRepresentable {
+    func makeUIViewController(context: Context) -> UIViewController {
+        UINavigationController(rootViewController: BaseTabBarController())
+    }
+    
+    func updateUIViewController(_ uiViewController: UIViewController, context: Context) {
+        
+    }
+    
+    typealias UIViewControllerType = UIViewController
+    
+    
+}
