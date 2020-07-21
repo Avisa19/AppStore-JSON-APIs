ac6a2792d3d0fba332ed7101cb74726a959d11a5
Avisa Poshtkouhi <aposhtkouhi@gmail.com>
2020-07-21 14:14:56 -0700

    Add UICollectionViewCell and resize them

--- AppStoreV1.1/AppStoreV1.1/Controllers/AppSearchController.swift
+++ AppStoreV1.1/AppStoreV1.1/Controllers/AppSearchController.swift
@@ -10,9 +10,22 @@ import SwiftUI
 
 class AppSearchController: UICollectionViewController {
     
+    fileprivate let CELL_ID = "CellId"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         collectionView.backgroundColor = UIColor.init(white: 0.5, alpha: 0.1)
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: CELL_ID)
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 7
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: CELL_ID, for: indexPath)
+        cell.backgroundColor = .systemRed
+        return cell
     }
     
     init() {
@@ -25,8 +38,15 @@ class AppSearchController: UICollectionViewController {
     }
 }
 
+extension AppSearchController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return .init(width: collectionView.frame.width, height: 200)
+    }
+}
+
 struct AppSearchController_Previews: PreviewProvider {
     static var previews: some View {
         Container().edgesIgnoringSafeArea(.all)
     }
 }
+
