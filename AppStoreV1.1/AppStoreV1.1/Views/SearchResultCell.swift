5406099f8c0596196d1bef34e052c38342916c15
Avisa Poshtkouhi <aposhtkouhi@gmail.com>
2020-07-21 14:54:24 -0700

    Add UI Elements to SearchResultCell

--- AppStoreV1.1/AppStoreV1.1/Views/SearchResultCell.swift
+++ AppStoreV1.1/AppStoreV1.1/Views/SearchResultCell.swift
@@ -10,6 +10,27 @@ import SwiftUI
 
 class SearchResultCell: UICollectionViewCell {
     
+    fileprivate let imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.backgroundColor = .systemTeal
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    fileprivate let nameLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Apps Name"
+        return label
+    }()
+    
+    fileprivate let getButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("GET", for: .normal)
+        button.setTitleColor(UIColor.systemBlue, for: .normal)
+        button.titleLabel?.font = .boldSystemFont(ofSize: 14)
+        return button
+    }()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         backgroundColor = .systemGreen
